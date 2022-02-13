80edcc301b144ace626d7cdc83f599fa52cec726
Nikuu <oskarradwan@wp.pl>
2022-02-13 14:40:32 +0100

    german and polish index site

---
+++ js/index-language.js
@@ -0,0 +1,55 @@
+if  (document.documentElement.lang == "en")
+{
+
+    document.getElementsByClassName("header-menu-item")[0].innerHTML="TAKE THE TEST";
+    document.getElementsByClassName("header-menu-item")[1].innerHTML="ABOUT";
+
+    document.getElementsByClassName("mobile-menu-link")[0].innerHTML="TAKE THE TEST";
+    document.getElementsByClassName("mobile-menu-link")[1].innerHTML="ABOUT";
+
+    document.getElementById("index-p").innerHTML="EN Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id odio tincidunt, bibendum elit vitae, elementum lacus. Sed accumsan nibh in lacus pellentesque, nec dapibus mi varius. Ut luctus sapien ac eros pulvinar congue. Sed in ipsum vitae nunc pharetra sollicitudin ac id arcu. Donec pulvinar, massa vel ultrices facilisis.";
+
+    document.getElementById("take-test-button").innerHTML="TAKE THE TEST";
+
+    document.getElementsByClassName("footer-link")[0].innerHTML="My GitHub";
+    document.getElementsByClassName("footer-link")[1].innerHTML="Website Map";
+
+}
+
+
+if  (document.documentElement.lang == "pl")
+{
+
+    document.getElementsByClassName("header-menu-item")[0].innerHTML="ROZPOCZNIJ";
+    document.getElementsByClassName("header-menu-item")[1].innerHTML="ABOUT";
+
+    document.getElementsByClassName("mobile-menu-link")[0].innerHTML="ROZPOCZNIJ";
+    document.getElementsByClassName("mobile-menu-link")[1].innerHTML="ABOUT";
+
+    document.getElementById("index-p").innerHTML="PL Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id odio tincidunt, bibendum elit vitae, elementum lacus. Sed accumsan nibh in lacus pellentesque, nec dapibus mi varius. Ut luctus sapien ac eros pulvinar congue. Sed in ipsum vitae nunc pharetra sollicitudin ac id arcu. Donec pulvinar, massa vel ultrices facilisis.";
+
+    document.getElementById("take-test-button").innerHTML="START";
+
+    document.getElementsByClassName("footer-link")[0].innerHTML="Mój GitHub";
+    document.getElementsByClassName("footer-link")[1].innerHTML="Mapa Strony";
+
+}
+
+
+if  (document.documentElement.lang == "de")
+{
+
+    document.getElementsByClassName("header-menu-item")[0].innerHTML="TAKE THE TEST";
+    document.getElementsByClassName("header-menu-item")[1].innerHTML="ABOUT";
+
+    document.getElementsByClassName("mobile-menu-link")[0].innerHTML="TAKE THE TEST";
+    document.getElementsByClassName("mobile-menu-link")[1].innerHTML="ABOUT";
+
+    document.getElementById("index-p").innerHTML="DE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id odio tincidunt, bibendum elit vitae, elementum lacus. Sed accumsan nibh in lacus pellentesque, nec dapibus mi varius. Ut luctus sapien ac eros pulvinar congue. Sed in ipsum vitae nunc pharetra sollicitudin ac id arcu. Donec pulvinar, massa vel ultrices facilisis.";
+
+    document.getElementById("take-test-button").innerHTML="TAKE THE TEST";
+
+    document.getElementsByClassName("footer-link")[0].innerHTML="Mein GitHub";
+    document.getElementsByClassName("footer-link")[1].innerHTML="Website Karte";
+
+}
